a31d3d1f67862d6f295de111c75ef2d3b5843741
Budi Jap <budi.jap@gmail.com>
2022-07-19 16:37:23 +1000

    Attempt to fix failing build on linux

--- cmake/_FindV8_.cmake
+++ cmake/_FindV8_.cmake
@@ -17,8 +17,7 @@ function(FindV8_Func)
                        ${V8_ROOT_DIR}/lib/libv8_libplatform.dylib
                        ${V8_ROOT_DIR}/lib/libv8.dylib)
    elseif (UNIX)
-      set(V8_LIBRARIES ${V8_ROOT_DIR}/lib/libc++.so
-                       ${V8_ROOT_DIR}/lib/libchrome_zlib.so
+      set(V8_LIBRARIES ${V8_ROOT_DIR}/lib/libchrome_zlib.so
                        ${V8_ROOT_DIR}/lib/libcppgc.so
                        ${V8_ROOT_DIR}/lib/libicui18n.so
                        ${V8_ROOT_DIR}/lib/libicuuc.so
@@ -32,8 +31,6 @@ function(FindV8_Func)
    endif(APPLE)
    add_library(${V8Proj} INTERFACE)
 
-   set (${V8Proj}_headers $<1:${V8_INCLUDE_DIR}/v8/v8.h>)
-   target_sources(${V8Proj} INTERFACE $<BUILD_INTERFACE:${${V8Proj}_headers}>)
    target_include_directories(${V8Proj} SYSTEM INTERFACE "$<BUILD_INTERFACE:${V8_INCLUDE_DIR}/v8>")
    target_link_libraries(${V8Proj} INTERFACE "$<BUILD_INTERFACE:${V8_LIBRARIES}>")
    target_compile_definitions(${V8Proj} INTERFACE V8_COMPRESS_POINTERS V8_ENABLE_SANDBOX)
